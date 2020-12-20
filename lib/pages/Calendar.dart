f55937314a9b7d8766c4c87452e48bebb2ce9bd2
rahmadiantio <t.rahmadian@gmail.com>
2020-12-20 13:48:10 +0700

    add calendar

--- lib/pages/Calendar.dart
+++ lib/pages/Calendar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatefulWidget {
   @override
@@ -7,11 +8,23 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
+  CalendarController _controller = CalendarController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Calendar"),
+      ),
+      body: Center(
+        child: TableCalendar(
+            calendarController: _controller,
+            availableGestures: AvailableGestures.horizontalSwipe,
+            calendarStyle: CalendarStyle(
+              weekdayStyle: TextStyle(color: Colors.blue),
+              weekendStyle: TextStyle(color: Colors.green),
+              outsideDaysVisible: false
+          ),
+        ),
       ),
     );
   }
