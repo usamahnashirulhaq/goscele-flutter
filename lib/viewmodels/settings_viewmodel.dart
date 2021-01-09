143e783ba891de5248b6bd5ada8eee93627adb64
usamah nashirulhaq <usamah1707@gmail.com>
2021-01-09 21:54:00 +0700

    Refactor UserDataService to UserDataRepository

--- lib/viewmodels/settings_viewmodel.dart
+++ lib/viewmodels/settings_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:goscele/repositories/user_data_repository.dart';
 import 'package:goscele/service_locator.dart';
 import 'package:goscele/services/auth_service.dart';
 import 'package:goscele/setup_dialog_ui.dart';
@@ -8,7 +9,7 @@ import 'package:stacked_services/stacked_services.dart';
 class SettingsViewModel extends BaseViewModel with FailureListener {
   static final _authService = locator<AuthService>();
   static final _dialogService = locator<DialogService>();
-  static final _userDataService = locator<UserDataService>();
+  static final _userDataRepository = locator<UserDataRepository>();
 
 
   Future<DialogResponse> _dialogResponse;
@@ -28,7 +29,7 @@ class SettingsViewModel extends BaseViewModel with FailureListener {
     );
   }
 
-  String getFullname() => "${_userDataService.firstName} ${_userDataService.lastName}";
+  String getFullname() => "${_userDataRepository.firstName} ${_userDataRepository.lastName}";
 
-  String getUsername() => "${_userDataService.username}";
+  String getUsername() => "${_userDataRepository.username}";
 }
